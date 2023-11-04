5927a58dc28dd7d0eb8f47f0954b54af55b9242b
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-04 21:05:07 +0530

    code.cpp

---
+++ code.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cost = 0;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n,x; // n = number of products
+        cin >> n >> x;
+        int a[n];
+        int b[n];
+        for(int i = 0;i<n;i++)
+        {
+            cin >> a[i];
+        }
+        for(int i = 0;i<n;i++)
+        {
+            cin >> b[i];
+        }
+       
+        for(int i = 0;i<n;i++)
+        {
+            if(a[i] >= x)
+            {
+                cost += b[i];
+            }
+        }
+        cout << cost << endl;
+    }
+	return 0;
+}
